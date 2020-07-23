7da22c6cdd502a88bfdda219150430c188fbb1d6
Robert Smayda <smayda44@gmail.com>
2020-07-23 19:11:45 -0400

    Initial commit (#1)

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+export const DEFAULT_SEARCH_RESULTS_PER_PAGE = 20;
+
+export const enum SEARCH_PAGINATION_PARAMS {
+    PAGES_OFFSET = '_getpagesoffset',
+    COUNT = '_count',
+}
+
+export const SEPARATOR: string = '_';
