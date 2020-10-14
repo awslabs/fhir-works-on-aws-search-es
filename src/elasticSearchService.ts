7d83b22842398fdc05ad676ac407169e63a64283
Tim Nguyen <thingut@amazon.com>
2020-10-13 20:24:27 -0400

    Check request.queryParams by key

--- src/elasticSearchService.ts
+++ src/elasticSearchService.ts
@@ -275,7 +275,7 @@ export class ElasticSearchService implements Search {
     ): Promise<SearchEntry[]> {
         if (
             !ITERATIVE_INCLUSION_PARAMETERS.some(param => {
-                return Object.keys(request.queryParams).includes(param);
+                return Object.prototype.hasOwnProperty.call(request.queryParams, param);
             })
         ) {
             return [];
