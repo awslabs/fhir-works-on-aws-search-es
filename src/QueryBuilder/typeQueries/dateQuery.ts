86dbccd6f8c75d90a0e0df15bbd908c030a84be7
Nestor Carvantes <carvantes@gmail.com>
2021-04-08 14:22:59 -0700

    feat: allow milliseconds in date queries (#55)

--- src/QueryBuilder/typeQueries/dateQuery.ts
+++ src/QueryBuilder/typeQueries/dateQuery.ts
@@ -21,14 +21,14 @@ interface DateSearchParameter {
 
 // The date parameter format is yyyy-mm-ddThh:mm:ss[Z|(+|-)hh:mm] (the standard XML format).
 // https://www.hl7.org/fhir/search.html#date
-const DATE_SEARCH_PARAM_REGEX = /^(?<prefix>eq|ne|lt|gt|ge|le|sa|eb|ap)?(?<inputDate>(?<year>\d{4})(?:-(?<month>\d{2})(?:-(?<day>\d{2})(?:T(?<hours>\d{2}):(?<minutes>\d{2})(?::(?<seconds>\d{2})(?<timeZone>Z|[+-](?:\d{2}:\d{2}))?)?)?)?)?)$/;
+const DATE_SEARCH_PARAM_REGEX = /^(?<prefix>eq|ne|lt|gt|ge|le|sa|eb|ap)?(?<inputDate>(?<year>\d{4})(?:-(?<month>\d{2})(?:-(?<day>\d{2})(?:T(?<hours>\d{2}):(?<minutes>\d{2})(?::(?<seconds>\d{2})(?:\.(?<milliseconds>\d{3}))?(?<timeZone>Z|[+-](?:\d{2}:\d{2}))?)?)?)?)?)$/;
 
 export const parseDateSearchParam = (param: string): DateSearchParameter => {
     const match = param.match(DATE_SEARCH_PARAM_REGEX);
     if (match === null) {
         throw new InvalidSearchParameterError(`Invalid date search parameter: ${param}`);
     }
-    const { inputDate, month, day, minutes, seconds } = match.groups!;
+    const { inputDate, month, day, minutes, seconds, milliseconds } = match.groups!;
 
     // If no prefix is present, the prefix eq is assumed.
     // https://www.hl7.org/fhir/search.html#prefix
@@ -42,11 +42,13 @@ export const parseDateSearchParam = (param: string): DateSearchParameter => {
     // When the date parameter is not fully specified, matches against it are based on the behavior of intervals
     // https://www.hl7.org/fhir/search.html#date
     let endDate: Date;
-    const timeEndOfDay = { hours: 23, minutes: 59, seconds: 59 };
-    if (seconds !== undefined) {
+    const timeEndOfDay = { hours: 23, minutes: 59, seconds: 59, milliseconds: 999 };
+    if (milliseconds !== undefined) {
         endDate = parsedDate; // date is fully specified
+    } else if (seconds !== undefined) {
+        endDate = set(parsedDate, { milliseconds: 999 });
     } else if (minutes !== undefined) {
-        endDate = set(parsedDate, { seconds: 59 });
+        endDate = set(parsedDate, { seconds: 59, milliseconds: 999 });
     } else if (day !== undefined) {
         endDate = set(parsedDate, timeEndOfDay);
     } else if (month !== undefined) {
