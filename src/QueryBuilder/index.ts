9bcdd81409daa6fcf60f431c1f38d9f9277d9095
ssvegaraju <ssvegaraju@yahoo.co.in>
2021-07-12 12:35:04 -0700

    Search Value now allows escaped commas, commas now indicate logical OR search

--- src/QueryBuilder/index.ts
+++ src/QueryBuilder/index.ts
@@ -69,16 +69,37 @@ function typeQueryWithConditions(
 }
 
 function searchParamQuery(searchParam: SearchParam, searchValue: string): any {
-    const queries = searchParam.compiled.map(compiled => {
-        return typeQueryWithConditions(searchParam, compiled, searchValue);
-    });
+    // split search value string based on commas for OR functionality unless escaped by \
+    let splitSearchValue : string[] = [];
+    let lastIndex = 0;
+    for (let c = 0; c < searchValue.length; c++) {
+        if (searchValue[c] === '\\') {
+            if (c + 1 < searchValue.length && searchValue[c+1] === ',') {
+                // replace the escape character to allow the string to be handled by ES
+                searchValue = searchValue.substring(0, c) + searchValue.substring(c+1);
+            }
+        } else if (searchValue[c] === ',') {
+            splitSearchValue.push(searchValue.substring(lastIndex, c));
+            lastIndex = c + 1;
+        }
+    }
+    splitSearchValue.push(searchValue.substring(lastIndex));
+    // construct queries for each split search value
+    let queryList = [];
+    for (let i = 0; i < splitSearchValue.length; i++) {
+        queryList.push(searchParam.compiled.map(compiled => {
+            return typeQueryWithConditions(searchParam, compiled, splitSearchValue[i]);
+        }));
+    }
+    // join queries
+    queryList.join();
 
-    if (queries.length === 1) {
-        return queries[0];
+    if (queryList.length === 1) {
+        return queryList[0];
     }
     return {
         bool: {
-            should: queries,
+            should: queryList,
         },
     };
 }
