ca8786faaca4a5fce3c4ec69526ca9579536cc85
rsmayda <smayda44@gmail.com>
2021-08-19 17:46:31 -0400

    test: add tests for `stringQuery`

---
+++ src/QueryBuilder/typeQueries/stringQuery.test.ts
@@ -0,0 +1,69 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+import { stringQuery } from './stringQuery';
+import { FHIRSearchParametersRegistry } from '../../FHIRSearchParametersRegistry';
+
+const fhirSearchParametersRegistry = new FHIRSearchParametersRegistry('4.0.1');
+const nameParam = fhirSearchParametersRegistry.getSearchParameter('Patient', 'name')!.compiled[0];
+
+describe('stringQuery', () => {
+    test('simple value', () => {
+        expect(stringQuery(nameParam, 'Robert Bell')).toMatchInlineSnapshot(`
+            Object {
+              "multi_match": Object {
+                "fields": Array [
+                  "name",
+                  "name.*",
+                ],
+                "lenient": true,
+                "query": "Robert Bell",
+              },
+            }
+        `);
+    });
+    test('simple value; with forward slash', () => {
+        expect(stringQuery(nameParam, 'Robert/Bobby Bell')).toMatchInlineSnapshot(`
+            Object {
+              "multi_match": Object {
+                "fields": Array [
+                  "name",
+                  "name.*",
+                ],
+                "lenient": true,
+                "query": "Robert\\\\/Bobby Bell",
+              },
+            }
+        `);
+    });
+    test('simple value; with backwards slash', () => {
+        expect(stringQuery(nameParam, 'Robert\\Bobby Bell')).toMatchInlineSnapshot(`
+            Object {
+              "multi_match": Object {
+                "fields": Array [
+                  "name",
+                  "name.*",
+                ],
+                "lenient": true,
+                "query": "Robert\\\\Bobby Bell",
+              },
+            }
+        `);
+    });
+    test('simple value; with characters', () => {
+        expect(stringQuery(nameParam, '平仮名')).toMatchInlineSnapshot(`
+            Object {
+              "multi_match": Object {
+                "fields": Array [
+                  "name",
+                  "name.*",
+                ],
+                "lenient": true,
+                "query": "平仮名",
+              },
+            }
+        `);
+    });
+});
