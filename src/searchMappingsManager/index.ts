a284116e470bd53e465a2f33188d737917486abe
Nestor Carvantes <carvantes@gmail.com>
2021-10-08 03:56:18 -0500

    address pr comments

--- src/searchMappingsManager/index.ts
+++ src/searchMappingsManager/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop,no-restricted-syntax */
 /*
  *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *  SPDX-License-Identifier: Apache-2.0
@@ -20,6 +21,13 @@ export class SearchMappingsManager {
 
     private readonly searchClient: Client;
 
+    /**
+     * @param options
+     * @param options.searchMappings - search mappings for all FHIR resource types
+     * @param options.numberOfShards - number of shards for each new index created. See the documentation for guidance on how to choose the right number:
+     * https://docs.aws.amazon.com/opensearch-service/latest/developerguide/sizing-domains.html#bp-sharding
+     * @param options.searchClient - optionally provide your own search client instance
+     */
     constructor({
         searchMappings,
         numberOfShards,
@@ -38,40 +46,49 @@ export class SearchMappingsManager {
      * Updates the mappings for all the FHIR resource types. If an index does not exist, it is created
      */
     async createOrUpdateMappings() {
-        const resourceTypesWithoutIndex: string[] = [];
-
-        // eslint-disable-next-line no-restricted-syntax
+        const resourceTypesWithErrors = [];
         for (const [resourceType, mappings] of Object.entries(this.searchMappings)) {
-            console.log(`sending putMapping request for: ${resourceType}`);
             try {
-                // eslint-disable-next-line no-await-in-loop
-                await this.searchClient.indices.put_mapping({
-                    index: toIndexName(resourceType),
-                    body: mappings,
-                });
-            } catch (error) {
-                if (error instanceof ResponseError && error.body.error.type === 'index_not_found_exception') {
-                    console.log(`index for ${resourceType} was not found. It will be created`);
-                    resourceTypesWithoutIndex.push(resourceType);
-                } else {
-                    throw error;
+                try {
+                    await this.updateMapping(resourceType, mappings);
+                } catch (error) {
+                    if (error instanceof ResponseError && error.body.error.type === 'index_not_found_exception') {
+                        console.log(`index for ${resourceType} was not found. It will be created`);
+                        await this.createIndexWithMapping(resourceType, mappings);
+                    } else {
+                        throw error;
+                    }
                 }
+            } catch (e) {
+                console.log(e);
+                console.log(`Failed to update mapping for ${resourceType}:`, JSON.stringify(e, null, 2));
+                resourceTypesWithErrors.push(resourceType);
             }
         }
 
-        // eslint-disable-next-line no-restricted-syntax
-        for (const resourceType of resourceTypesWithoutIndex) {
-            console.log(`creating index for ${resourceType}`);
-            // eslint-disable-next-line no-await-in-loop
-            await this.searchClient.indices.create({
-                index: toIndexName(resourceType),
-                body: {
-                    mappings: this.searchMappings[resourceType],
-                    settings: {
-                        number_of_shards: this.numberOfShards,
-                    },
-                },
-            });
+        if (resourceTypesWithErrors.length > 0) {
+            throw new Error(`Failed to update mappings for: ${resourceTypesWithErrors}`);
         }
+    }
+
+    async updateMapping(resourceType: string, mapping: any) {
+        console.log(`sending putMapping request for: ${resourceType}`);
+        return this.searchClient.indices.put_mapping({
+            index: toIndexName(resourceType),
+            body: mapping,
+        });
+    }
+
+    async createIndexWithMapping(resourceType: string, mapping: any) {
+        console.log(`creating index for ${resourceType}`);
+        return this.searchClient.indices.create({
+            index: toIndexName(resourceType),
+            body: {
+                mappings: mapping,
+                settings: {
+                    number_of_shards: this.numberOfShards,
+                },
+            },
+        });
     }
 }
