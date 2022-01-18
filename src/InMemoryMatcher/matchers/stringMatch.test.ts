1884c6b1ff3354a7c1d112cd759a6611908ed513
Nestor Carvantes <carvantes@gmail.com>
2022-01-18 03:19:04 -0600

    feat: add string, token, uri matchers

--- src/InMemoryMatcher/matchers/stringMatch.test.ts
+++ src/InMemoryMatcher/matchers/stringMatch.test.ts
@@ -5,16 +5,43 @@
  */
 
 import { stringMatch } from './stringMatch';
+import { CompiledSearchParam } from '../../FHIRSearchParametersRegistry';
+
+const COMPILED_SEARCH_PARAM: CompiledSearchParam = { path: 'someField', resourceType: 'someResource' };
 
 describe('stringMatch', () => {
     test('matches string', () => {
-        expect(stringMatch('hello', 'hello')).toBe(true);
-        expect(stringMatch('hello', 'something else')).toBe(false);
+        expect(stringMatch(COMPILED_SEARCH_PARAM, 'hello', 'hello')).toBe(true);
+        expect(stringMatch(COMPILED_SEARCH_PARAM, 'hello', 'something else')).toBe(false);
     });
 
     test('not a string', () => {
-        expect(stringMatch('hello', [])).toBe(false);
-        expect(stringMatch('hello', {})).toBe(false);
-        expect(stringMatch('hello', 23.1)).toBe(false);
+        expect(stringMatch(COMPILED_SEARCH_PARAM, 'hello', [])).toBe(false);
+        expect(stringMatch(COMPILED_SEARCH_PARAM, 'hello', {})).toBe(false);
+        expect(stringMatch(COMPILED_SEARCH_PARAM, 'hello', 23.1)).toBe(false);
+    });
+
+    describe('special cases', () => {
+        describe('name', () => {
+            test.each(['family', 'given', 'text', 'prefix', 'suffix'])('%p field', (field) => {
+                const compiledNameParam = { path: 'name', resourceType: 'someResource' };
+                expect(
+                    stringMatch(compiledNameParam, 'John', {
+                        [field]: 'John',
+                    }),
+                ).toBe(true);
+            });
+        });
+
+        describe('address', () => {
+            test.each(['city', 'country', 'district', 'line', 'postalCode', 'state', 'text'])('%p field', (field) => {
+                const compiledNameParam = { path: 'address', resourceType: 'someResource' };
+                expect(
+                    stringMatch(compiledNameParam, 'somePlace', {
+                        [field]: 'somePlace',
+                    }),
+                ).toBe(true);
+            });
+        });
     });
 });
--- src/InMemoryMatcher/matchers/stringMatch.ts
+++ src/InMemoryMatcher/matchers/stringMatch.ts
@@ -5,11 +5,41 @@
  */
 
 import { StringLikeSearchValue } from '../../FhirQueryParser';
+import { CompiledSearchParam } from '../../FHIRSearchParametersRegistry';
 
 // eslint-disable-next-line import/prefer-default-export
-export const stringMatch = (value: StringLikeSearchValue, resourceValue: any): boolean => {
-    if (typeof resourceValue === 'string') {
-        return value === resourceValue;
+export const stringMatch = (
+    compiledSearchParam: CompiledSearchParam,
+    value: StringLikeSearchValue,
+    resourceValue: any,
+): boolean => {
+    if (compiledSearchParam.path === 'name') {
+        // name is a special parameter.
+        const nameFields = [
+            resourceValue?.family,
+            resourceValue?.given,
+            resourceValue?.text,
+            resourceValue?.prefix,
+            resourceValue?.suffix,
+        ];
+
+        return nameFields.some((f) => f === value);
     }
-    return false;
+
+    if (compiledSearchParam.path === 'address') {
+        // address is a special parameter.
+        const addressFields = [
+            resourceValue?.city,
+            resourceValue?.country,
+            resourceValue?.district,
+            resourceValue?.line,
+            resourceValue?.postalCode,
+            resourceValue?.state,
+            resourceValue?.text,
+        ];
+
+        return addressFields.some((f) => f === value);
+    }
+
+    return value === resourceValue;
 };
