8c68539d54896a3478d301712e83d84919e66235
Nestor Carvantes <carvantes@gmail.com>
2022-01-21 19:17:06 -0600

    refactor: add referenceParser (#160)

--- src/QueryBuilder/typeQueries/referenceQuery.test.ts
+++ src/QueryBuilder/typeQueries/referenceQuery.test.ts
@@ -6,6 +6,7 @@
 import { InvalidSearchParameterError } from 'fhir-works-on-aws-interface';
 import { referenceQuery } from './referenceQuery';
 import { FHIRSearchParametersRegistry } from '../../FHIRSearchParametersRegistry';
+import { parseReferenceSearchValue } from '../../FhirQueryParser/typeParsers/referenceParser';
 
 const fhirSearchParametersRegistry = new FHIRSearchParametersRegistry('4.0.1');
 const organizationParam = fhirSearchParametersRegistry.getSearchParameter('Patient', 'organization')!.compiled[0];
@@ -14,30 +15,44 @@ describe('referenceQuery', () => {
     describe('searching with {type}/{id}', () => {
         test('keyword included', () => {
             expect(
-                referenceQuery(organizationParam, 'Organization/111', true, 'https://base-url.com', 'organization', []),
+                referenceQuery(
+                    organizationParam,
+                    parseReferenceSearchValue({ name: 'organization', target: [] }, 'Organization/111'),
+                    true,
+                    'https://base-url.com',
+                    'organization',
+                    [],
+                ),
             ).toMatchInlineSnapshot(`
-              Object {
-                "terms": Object {
-                  "managingOrganization.reference.keyword": Array [
-                    "Organization/111",
-                    "https://base-url.com/Organization/111",
-                  ],
-                },
-              }
-            `);
+                              Object {
+                                "terms": Object {
+                                  "managingOrganization.reference.keyword": Array [
+                                    "Organization/111",
+                                    "https://base-url.com/Organization/111",
+                                  ],
+                                },
+                              }
+                        `);
         });
         test('keyword not included', () => {
-            expect(referenceQuery(organizationParam, 'Organization/111', false, 'https://base-url.com', 'organization'))
-                .toMatchInlineSnapshot(`
-              Object {
-                "terms": Object {
-                  "managingOrganization.reference": Array [
-                    "Organization/111",
-                    "https://base-url.com/Organization/111",
-                  ],
-                },
-              }
-            `);
+            expect(
+                referenceQuery(
+                    organizationParam,
+                    parseReferenceSearchValue({ name: 'organization', target: [] }, 'Organization/111'),
+                    false,
+                    'https://base-url.com',
+                    'organization',
+                ),
+            ).toMatchInlineSnapshot(`
+                              Object {
+                                "terms": Object {
+                                  "managingOrganization.reference": Array [
+                                    "Organization/111",
+                                    "https://base-url.com/Organization/111",
+                                  ],
+                                },
+                              }
+                        `);
         });
     });
     describe('searching with {fhirServiceBaseUrl}/{type}/{id}', () => {
@@ -45,81 +60,105 @@ describe('referenceQuery', () => {
             expect(
                 referenceQuery(
                     organizationParam,
-                    'https://base-url.com/Organization/111',
+                    parseReferenceSearchValue(
+                        { name: 'organization', target: [] },
+                        'https://base-url.com/Organization/111',
+                    ),
                     true,
                     'https://base-url.com',
                     'organization',
                 ),
             ).toMatchInlineSnapshot(`
-              Object {
-                "terms": Object {
-                  "managingOrganization.reference.keyword": Array [
-                    "https://base-url.com/Organization/111",
-                    "Organization/111",
-                  ],
-                },
-              }
+                Object {
+                  "terms": Object {
+                    "managingOrganization.reference.keyword": Array [
+                      "Organization/111",
+                      "https://base-url.com/Organization/111",
+                    ],
+                  },
+                }
             `);
         });
         test('fhirServiceBaseUrl does not match baseUrl', () => {
             expect(
                 referenceQuery(
                     organizationParam,
-                    'http://notMatching.com/baseR4/Organization/111',
+                    parseReferenceSearchValue(
+                        { name: 'organization', target: [] },
+                        'http://notMatching.com/baseR4/Organization/111',
+                    ),
                     true,
                     'https://base-url.com',
                     'organization',
                 ),
             ).toMatchInlineSnapshot(`
-              Object {
-                "terms": Object {
-                  "managingOrganization.reference.keyword": Array [
-                    "http://notMatching.com/baseR4/Organization/111",
-                  ],
-                },
-              }
-            `);
+                              Object {
+                                "terms": Object {
+                                  "managingOrganization.reference.keyword": Array [
+                                    "http://notMatching.com/baseR4/Organization/111",
+                                  ],
+                                },
+                              }
+                        `);
         });
     });
     describe('searching with just {id}', () => {
         test('one target type found', () => {
             expect(
-                referenceQuery(organizationParam, 'organizationId', true, 'https://base-url.com', 'organization', [
-                    'Organization',
-                ]),
+                referenceQuery(
+                    organizationParam,
+                    parseReferenceSearchValue({ name: 'organization', target: ['Organization'] }, 'organizationId'),
+                    true,
+                    'https://base-url.com',
+                    'organization',
+                    ['Organization'],
+                ),
             ).toMatchInlineSnapshot(`
-              Object {
-                "terms": Object {
-                  "managingOrganization.reference.keyword": Array [
-                    "https://base-url.com/Organization/organizationId",
-                    "Organization/organizationId",
-                  ],
-                },
-              }
-            `);
+                              Object {
+                                "terms": Object {
+                                  "managingOrganization.reference.keyword": Array [
+                                    "https://base-url.com/Organization/organizationId",
+                                    "Organization/organizationId",
+                                  ],
+                                },
+                              }
+                        `);
         });
         test('many target types found', () => {
             expect(
-                referenceQuery(organizationParam, 'organizationId', true, 'https://base-url.com', 'organization', [
-                    'Organization',
-                    'Group',
-                ]),
+                referenceQuery(
+                    organizationParam,
+                    parseReferenceSearchValue(
+                        { name: 'organization', target: ['Organization', 'Group'] },
+                        'organizationId',
+                    ),
+                    true,
+                    'https://base-url.com',
+                    'organization',
+                    ['Organization', 'Group'],
+                ),
             ).toMatchInlineSnapshot(`
-              Object {
-                "terms": Object {
-                  "managingOrganization.reference.keyword": Array [
-                    "https://base-url.com/Organization/organizationId",
-                    "Organization/organizationId",
-                    "https://base-url.com/Group/organizationId",
-                    "Group/organizationId",
-                  ],
-                },
-              }
-            `);
+                              Object {
+                                "terms": Object {
+                                  "managingOrganization.reference.keyword": Array [
+                                    "https://base-url.com/Organization/organizationId",
+                                    "Organization/organizationId",
+                                    "https://base-url.com/Group/organizationId",
+                                    "Group/organizationId",
+                                  ],
+                                },
+                              }
+                        `);
         });
         test('no target types found', () => {
             expect(() =>
-                referenceQuery(organizationParam, 'organizationId', false, 'https://base-url.com', 'organization'),
+                referenceQuery(
+                    organizationParam,
+                    parseReferenceSearchValue({ name: 'organization', target: [] }, 'organizationId'),
+                    false,
+                    'https://base-url.com',
+                    'organization',
+                ),
             ).toThrow(InvalidSearchParameterError);
         });
     });
@@ -127,7 +166,7 @@ describe('referenceQuery', () => {
         expect(() =>
             referenceQuery(
                 organizationParam,
-                'organizationId',
+                parseReferenceSearchValue({ name: 'organization', target: ['Organization'] }, 'organizationId'),
                 false,
                 'https://base-url.com',
                 'organization',
@@ -136,12 +175,17 @@ describe('referenceQuery', () => {
             ),
         ).toThrow(InvalidSearchParameterError);
     });
+
     test('search value is not an URL nor has the format <resourceType>/<id>', () => {
         expect(
-            referenceQuery(organizationParam, 'this:does# not match', true, 'https://base-url.com', 'organization', [
-                'Organization',
-                'Group',
-            ]),
+            referenceQuery(
+                organizationParam,
+                parseReferenceSearchValue({ name: 'organization', target: [] }, 'this:does# not match'),
+                true,
+                'https://base-url.com',
+                'organization',
+                ['Organization', 'Group'],
+            ),
         ).toMatchInlineSnapshot(`
           Object {
             "terms": Object {
