b1e3a1aeba2a84b7f5d080ded4024bcb88169c0a
Sukeerth Vegaraju <ssvegaraju@yahoo.co.in>
2022-01-31 18:10:03 -0800

    fix: allow revinclude to return more than 10 resources (#164)

--- src/constants.ts
+++ src/constants.ts
@@ -26,3 +26,5 @@ export const NON_SEARCHABLE_PARAMETERS = [
 export const MAX_ES_WINDOW_SIZE: number = 10000;
 
 export const MAX_CHAINED_PARAMS_RESULT: number = 100;
+
+export const MAX_INCLUSION_PARAM_RESULTS: number = 1000;
