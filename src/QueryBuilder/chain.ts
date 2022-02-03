bc805cb3339a29d2f5c80bdb9a08ce425c90e752
Sukeerth Vegaraju <ssvegaraju@yahoo.co.in>
2022-02-03 15:16:09 -0800

    fix: chain parameters should inspect conditions to narrow down possible target types (#168)

--- src/QueryBuilder/chain.ts
+++ src/QueryBuilder/chain.ts
@@ -1,12 +1,44 @@
 // eslint-disable-next-line import/prefer-default-export
 import { InvalidSearchParameterError } from 'fhir-works-on-aws-interface';
-import { FHIRSearchParametersRegistry } from '../FHIRSearchParametersRegistry';
-import { NON_SEARCHABLE_PARAMETERS } from '../constants';
+import { FHIRSearchParametersRegistry, SearchParam } from '../FHIRSearchParametersRegistry';
+import { COMPILED_CONDITION_OPERATOR_RESOLVE, NON_SEARCHABLE_PARAMETERS } from '../constants';
 import { parseSearchModifiers, normalizeQueryParams, isChainedParameter } from '../FhirQueryParser/util';
 
 export interface ChainParameter {
     chain: { resourceType: string; searchParam: string }[];
     initialValue: string[];
+}
+
+export function getUniqueTarget(fhirSearchParam: SearchParam): string | undefined {
+    if (!fhirSearchParam.target) {
+        return undefined;
+    }
+    if (fhirSearchParam.target.length === 1) {
+        return fhirSearchParam.target[0];
+    }
+    let target: string | undefined;
+    for (let i = 0; i < fhirSearchParam.compiled.length; i += 1) {
+        // check compiled[].condition for resolution
+        const compiled = fhirSearchParam.compiled[i]; // we can use ! since we checked length before
+        // condition's format is defined in `../FHIRSearchParamtersRegistry/index.ts`
+        if (compiled.condition && compiled.condition[1] === COMPILED_CONDITION_OPERATOR_RESOLVE) {
+            if (!target) {
+                // eslint-disable-next-line prefer-destructuring
+                target = compiled.condition[2];
+            } else if (target !== compiled.condition[2]) {
+                // case where two compiled resolve to different resource types
+                return undefined;
+            }
+        } else {
+            // if there is no resolve condition, we have multiple resources pointed to.
+            return undefined;
+        }
+    }
+    // case for resolution to resource type that isn't contained in the target group
+    if (target && !fhirSearchParam.target.includes(target)) {
+        return undefined;
+    }
+    return target;
 }
 
 const parseChainedParameters = (
@@ -54,16 +86,18 @@ const parseChainedParameters = (
                             `Chained search parameter '${searchModifier.parameterName}' for resource type ${currentResourceType} does not point to resource type ${searchModifier.modifier}.`,
                         );
                     }
-                } else if (fhirSearchParam.target?.length !== 1) {
-                    throw new InvalidSearchParameterError(
-                        `Chained search parameter '${searchModifier.parameterName}' for resource type ${currentResourceType} points to multiple resource types, please specify.`,
-                    );
                 } else {
+                    const target = getUniqueTarget(fhirSearchParam);
+                    if (!target) {
+                        throw new InvalidSearchParameterError(
+                            `Chained search parameter '${searchModifier.parameterName}' for resource type ${currentResourceType} points to multiple resource types, please specify.`,
+                        );
+                    }
                     organizedChain.push({
                         resourceType: currentResourceType,
                         searchParam: searchModifier.parameterName,
                     });
-                    [nextResourceType] = fhirSearchParam.target;
+                    nextResourceType = target;
                 }
                 currentResourceType = nextResourceType;
             });
